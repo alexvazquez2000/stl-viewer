03903c6633ea1f4e8a5a162625894f265aaaf50f
Alex Vazquez <101617394+alexvazquez2000@users.noreply.github.com>
2025-11-13 13:03:31 -0700

    Tests for ascii and binary STL files

---
+++ src/test/java/hall/collin/christopher/stl4j/STLParserTest.java
@@ -0,0 +1,34 @@
+package hall.collin.christopher.stl4j;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class STLParserTest {
+
+	@Test
+	void testParseAsciiStlFile() throws IllegalArgumentException, IOException, URISyntaxException {
+		URL fileUrl = getClass().getResource("/cube_ascii.stl");
+		File f = new File(fileUrl.toURI());
+		// read file to array of triangles
+		List<Triangle> mesh = new STLParser().parseSTLFile(f.toPath());
+		assertEquals(4, mesh.size());
+	}
+	
+
+	@Test
+	void testParseBinaryStlFile() throws IllegalArgumentException, IOException, URISyntaxException {
+		URL fileUrl = getClass().getResource("/3D_model_of_a_Cube.stl");
+		File f = new File(fileUrl.toURI());
+		// read file to array of triangles
+		List<Triangle> mesh = new STLParser().parseSTLFile(f.toPath());
+		assertEquals(12, mesh.size());
+	}
+
+}
