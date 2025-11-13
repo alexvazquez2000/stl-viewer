a5bddfaa243bdf365195190dab922be43c25e164
Alex Vazquez <101617394+alexvazquez2000@users.noreply.github.com>
2025-11-13 11:44:29 -0700

    Add JUnit support. New TDD test for Triangle class

---
+++ src/test/java/hall/collin/christopher/stl4j/TriangleTest.java
@@ -0,0 +1,44 @@
+package hall.collin.christopher.stl4j;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TriangleTest {
+
+	@Test
+	void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTriangle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTranslate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetVertices() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNormal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+}
